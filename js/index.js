6c385d1841eb891ea85a455a341d876b5aed9875
ElliotXLeo <elliotgaramendi@gmail.com>
2021-11-29 15:51:23 -0500

    Mejora de estructura, estilos e ingreso de nombre

---
+++ js/index.js
@@ -0,0 +1,21 @@
+'use strict';
+
+let partner = prompt('🥰 Dyme 2 naMe 🥰');
+document.querySelector('.contenedor__titulo').innerHTML += partner + ' ❤';
+
+const zomosNobios = () => {
+  // alert('💞Now zomos ParTnEr ' + ${partner} + '💞');
+  alert(`💞Now zomos ParTnEr ${partner} 💞`);
+  alert('😱Nuestra VoDa Is MañaNa😱');
+  location.href = "https://youtu.be/am1_JLFDFMw?t=19";
+};
+
+const ebitarKCRompaMyBobo = () => {
+  votonNo.style.position = 'absolute';
+  votonNo.style.top = (Math.random() * window.innerHeight) + 'px';
+  votonNo.style.left = (Math.random() * window.innerWidth) + 'px';
+};
+
+window.document.getElementById('yes').onclick = zomosNobios;
+let votonNo = window.document.getElementById('no');
+votonNo.onmouseover = ebitarKCRompaMyBobo;
